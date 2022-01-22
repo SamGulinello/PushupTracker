715d812e5bf3b594a9502ae604982520f5560a30
SamGulinello <sgulinello12@gmail.com>
2022-01-22 17:12:29 -0500

    Initial Commit

---
+++ Pushup Tracker/Pushup_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pushup_TrackerApp.swift
+//  Pushup Tracker
+//
+//  Created by Sam Gulinello on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Pushup_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
